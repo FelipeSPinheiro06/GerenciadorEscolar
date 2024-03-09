8f9a85d94d416e0978f1f409bbe659cf18f95143
FelipePinheiro <fsp12371@git.com>
2024-03-09 12:07:28 -0300

    Done - AlunoController

--- src/main/java/com/fiap/gerenciadorescolar/Controller/AlunoController.java
+++ src/main/java/com/fiap/gerenciadorescolar/Controller/AlunoController.java
@@ -2,13 +2,17 @@ package com.fiap.gerenciadorescolar.Controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -33,73 +37,68 @@ public class AlunoController {
     @PostMapping()
     @ResponseStatus(code = HttpStatus.CREATED)
     public Aluno AdicionarAluno(@RequestBody Aluno aluno) {
-        log.info("cadastrando aluno" + categoria);
+        log.info("cadastrando aluno" + aluno);
         repository.add(aluno);
         return aluno;
     }
 
     @GetMapping("{rm}")
-    public ResponseEntity<Categoria> show(@PathVariable Long Rm){
-        log.info("buscar aluno por id {} ", id);
+    public ResponseEntity<Aluno> show(@PathVariable Long rm){
+        log.info("buscar aluno por id {} ", rm);
 
         /*for(Aluno aluno : repository){
             if (categoria.rm().equals(rm))
                 return ResponseEntity.ok(categoria);
         }*/
                                                                      //FindFirst vai pegar o elemento do filtro que passou
-        var alunoEncontrado = repository
-        .stream()
-        .filter( a->  a.rm().equals(rm))
-        .findFirst();
+        var alunoEncontrado = getAlunoByID(rm);
                                 //se estiver vazia
-        if(categoriaEncontrada.isEmpty())
+        if(alunoEncontrado.isEmpty())
             return ResponseEntity.notFound().build();
 
-        return ResponseEntity.ok(alunoEncontrado.get() );
-
-        
-
-      
+        return ResponseEntity.ok(alunoEncontrado.get());
     }
-    @DeleteMaping("{rm}")
-    public ResponseEntity<Object> destroy(@PAthvAriable Long id){
+    
+    @DeleteMapping("{rm}")
+    public ResponseEntity<Object> destroy(@PathVariable Long rm){
         log.info("Apagando categoria com rm {}", rm);
 
-        var alunoEncontrado = repository
-        .stream()
-        .filter( a->  a.rm().equals(rm))
-        .findFirst();
-        if (alunoEncontrado.isEmpty)
+        var alunoEncontrado = getAlunoByID(rm);
+        
+        if (alunoEncontrado.isEmpty())
             return ResponseEntity.notFound().build();
 
         repository.remove(alunoEncontrado.get());
 
-        return responseENtity.noContent().build();
+        return ResponseEntity.noContent().build();
     }
-    @putMaping("{rm}")
+    
+    
+    @PutMapping("{rm}")
     public ResponseEntity<Aluno> atualizar (@PathVariable Long rm, @RequestBody Aluno aluno){
         log.info("Atualizando aluno com rm {} para {}", rm, aluno);
 
-        var alunoEncontrado = repository
-        .stream()
-        .filter( a->  a.rm().equals(rm))
-        .findFirst();
-        if (alunoEncontrado.isEmpty)
+        var alunoEncontrado = getAlunoByID(rm);
+        if (alunoEncontrado.isEmpty())
             return ResponseEntity.notFound().build();
-        var AlunoAntigo = alunoEncontrado.get();
-        var AlunoNovo = new Categoria(rm ,aluno.nome(), aluno.turma());
-
-        repository.remove(AlunoAntigo);
-        repository.add(categoriaNova);
-
-        return ResponseEntity
-
         
-        
+        var alunoAntigo = alunoEncontrado.get();
+        var alunoNovo = new Aluno(rm ,aluno.getNome(), aluno.getTurma());
 
+        repository.remove(alunoAntigo);
+        repository.add(alunoNovo);
+
+        return ResponseEntity.ok(alunoNovo);
     }
     
-    
+    private Optional<Aluno> getAlunoByID(Long rm) {
+        var alunoEncontrado = repository
+        .stream()
+        .filter( a -> a.getRm().equals(rm))
+        .findFirst();
+
+        return alunoEncontrado;
+    }    
 
     
 }
