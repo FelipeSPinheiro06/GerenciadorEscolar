4fd5bd9afd3e3a71998e709341b717ff41439ebc
FelipePinheiro <fsp12371@git.com>
2024-04-30 17:24:30 -0300

    Construtor removido

--- src/main/java/com/fiap/gerenciadorescolar/Model/Aluno.java
+++ src/main/java/com/fiap/gerenciadorescolar/Model/Aluno.java
@@ -1,19 +1,19 @@
 package com.fiap.gerenciadorescolar.model;
 
-import java.util.Random;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @Entity
 @NoArgsConstructor
+@AllArgsConstructor
 public class Aluno {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,10 +25,4 @@ public class Aluno {
     
     @NotNull(message = "{aluno.nulo}")
     private String turma;
-
-    public Aluno(Long rm, String nome, String turma){
-        this.rm = Math.abs(new Random().nextLong());
-        this.nome = nome;
-        this.turma = turma;
-    }
 }
